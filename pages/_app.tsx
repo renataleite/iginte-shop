d48bb11f91b36906aeca3556923409869c5bfe23
Renata <renataleitesg@gmail.com>
2022-12-11 13:19:02 -0300

    add the global styles

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
-
-globalStyles();
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
